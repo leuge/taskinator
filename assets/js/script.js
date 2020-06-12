65ce6c876cfe249766f874806b04fa54de2aacc6
Windy <laureneuge@Laurens-MBP-17.home>
2020-06-11 21:49:41 -0700

    style to drag feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -203,7 +203,9 @@ var taskStatusChangeHandler = function(event) {
 var dragTaskHandler = function(event) {
   var taskId = event.target.getAttribute("data-task-id");
   event.dataTransfer.setData("text/plain", taskId);
-  var getId = event.dataTransfer.getData("text/plain");
+  var getId = event.dataTransfer.getData
+  dropZoneEl.removeAttribute("style");("text/plain");
+
 console.log("getId:", getId, typeof getId);
 } 
 
@@ -211,6 +213,7 @@ var dropZoneDragHandler = function(event) {
   var taskListEl = event.target.closest(".task-list");
   if (taskListEl) {
   event.preventDefault();
+  taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
   }
 };
 
@@ -233,8 +236,16 @@ var dropTaskHandler = function(event) {
   dropZoneEl.appendChild(draggableElement);
 };
 
+var dragLeaveHandler = function(event) {
+  var taskListEl = event.target.closest(".task-list");
+  if (taskListEl) {
+    taskListEl.removeAttribute("style");
+  }
+}
+
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
